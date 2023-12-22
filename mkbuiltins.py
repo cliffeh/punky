01f14a757436b346f63b0b3c1fcc0b56af6874a8
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-22 12:13:13 -0500

    generifying mkbuiltins.py

--- mkbuiltins.py
+++ mkbuiltins.py
@@ -12,7 +12,7 @@ BUILTINS = [
     ("LAMBDA", None,    "lambda", "[lL][aA][mM][bB][dD][aA]"),
 ]
 
-BUILTIN_TYPEDEF_MACRO = '@BUILTIN_TYPEDEFS@'
+BUILTIN_TYPEDEFS_MACRO = '@BUILTIN_TYPEDEFS@'
 BUILTIN_EXTERN_MACRO = '@BUILTIN_EXTERNS@'
 BUILTIN_PATTERN_MACRO = '@BUILTIN_PATTERNS@'
 
@@ -34,40 +34,24 @@ def print_externs():
         print(f'extern sexpr B_{builtin[0]};')
 
 
-def generate_punky_h(f):
-    for line in f:
-        line = line.rstrip()
-        if BUILTIN_TYPEDEF_MACRO in line:
-            print_typedefs()
-        elif BUILTIN_EXTERN_MACRO in line:
-            print_externs()
-        else:
-            print(line)
-
-
-def print_scan_patterns():
+def print_scanner_patterns():
     for builtin in BUILTINS:
         print(
             f'{builtin[3]} {{ *yylval = &B_{builtin[0]}; return(BUILTIN); }}')
 
 
-def generate_scan_l(f):
-    for line in f:
+def replace_macros():
+    for line in sys.stdin:
         line = line.rstrip()
-        if line == BUILTIN_PATTERN_MACRO:
-            print_scan_patterns()
+        if BUILTIN_TYPEDEFS_MACRO in line:
+            print_typedefs()
+        elif BUILTIN_EXTERN_MACRO in line:
+            print_externs()
+        elif BUILTIN_PATTERN_MACRO in line:
+            print_scanner_patterns()
         else:
             print(line)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f'usage: {sys.argv[0]} punky.h.in|scan.l.in')
-        exit(1)
-
-    if sys.argv[1] == 'punky.h.in':
-        with open('punky.h.in') as f:
-            generate_punky_h(f)
-    elif sys.argv[1] == 'scan.l.in':
-        with open('scan.l.in') as f:
-            generate_scan_l(f)
+    replace_macros()

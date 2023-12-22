6ca65dacdcc035a1e6c236cf75e04c44edc4f2b9
Cliff Snyder <cliff.snyder@gmail.com>
2023-12-22 12:07:10 -0500

    refactoring to make linter happy

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ a simple functional language interpreter
 ## Notes
 To add a new builtin type you have to:
 
-  1) add a builtin_type and an extern for it (punky.h)
+  1) add a tuple to BUILTINS in mkbuiltins.py
   2) add a sexpr, a case statement, and an apply function for it (builtin.c)
   2) add it to the scanner (scan.l)
--- mkbuiltins.py
+++ mkbuiltins.py
@@ -12,40 +12,44 @@ BUILTINS = [
     ("LAMBDA", None,    "lambda", "[lL][aA][mM][bB][dD][aA]"),
 ]
 
-BUILTIN_TYPEDEF_MACRO = '@BUILTIN_TYPEDEF@'
+BUILTIN_TYPEDEF_MACRO = '@BUILTIN_TYPEDEFS@'
 BUILTIN_EXTERN_MACRO = '@BUILTIN_EXTERNS@'
 BUILTIN_PATTERN_MACRO = '@BUILTIN_PATTERNS@'
 
+
 def print_typedefs():
-    print('typedef enum builtin_type\n{')
-    typedefs = ['  B_TYPE_NIL = 0']  # NIL special case
+    typedefs = []
     for builtin in BUILTINS:
         typedef = f'B_TYPE_{builtin[0]}'
         if builtin[1] is not None:
             typedef += f' = \'{builtin[1]}\''
         typedefs.append(typedef)
 
-    print(',\n  '.join(typedefs))
-    print('} builtin_type;')
+    print('  ' + ',\n  '.join(typedefs))
+
 
 def print_externs():
     print('extern sexpr NIL;')  # NIL special case
     for builtin in BUILTINS:
         print(f'extern sexpr B_{builtin[0]};')
 
+
 def generate_punky_h(f):
     for line in f:
         line = line.rstrip()
-        if line == BUILTIN_TYPEDEF_MACRO:
+        if BUILTIN_TYPEDEF_MACRO in line:
             print_typedefs()
-        elif line == BUILTIN_EXTERN_MACRO:
+        elif BUILTIN_EXTERN_MACRO in line:
             print_externs()
         else:
             print(line)
 
+
 def print_scan_patterns():
     for builtin in BUILTINS:
-        print(f'{builtin[3]} {{ *yylval = &B_{builtin[0]}; return(BUILTIN); }}')
+        print(
+            f'{builtin[3]} {{ *yylval = &B_{builtin[0]}; return(BUILTIN); }}')
+
 
 def generate_scan_l(f):
     for line in f:
@@ -54,6 +58,7 @@ def generate_scan_l(f):
             print_scan_patterns()
         else:
             print(line)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:

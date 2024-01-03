5fe0ad28435a3d759445d00e8ed561b127f0a226
Cliff Snyder <cliff.snyder@gmail.com>
2024-01-03 09:08:00 -0500

    refactoring env to use sexprs

--- env.h
+++ env.h
@@ -2,23 +2,13 @@
 
 #include "types.h"
 
-// TODO something better than linear search through a linked list
-typedef struct entry
-{
-  char *key;
-  sexpr *value;
-  struct entry *next;
-} entry;
-
 typedef struct environment
 {
-  int count;
-  entry *entries;
+  sexpr *entries;
   struct environment *parent;
 } environment;
 
 void env_init (environment *env, environment *parent);
 sexpr *env_get (environment *env, const char *key);
 void env_set (environment *env, const char *key, sexpr *e);
-sexpr *env_del (environment *env, const char *key);
 void env_destroy (environment *env);
